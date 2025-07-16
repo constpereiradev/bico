36bd8f8c4a052e5939b75cca44bd146a4c3277bb
constpereiradev <amandapereiradevcontact@gmail.com>
2025-07-16 10:11:31 -0300

    feat: translated and ajusted usermenucontent

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -25,17 +25,18 @@ defineProps<Props>();
     <DropdownMenuSeparator />
     <DropdownMenuGroup>
         <DropdownMenuItem :as-child="true">
-            <Link class="block w-full" :href="route('profile.edit')" prefetch as="button">
-                <Settings class="mr-2 h-4 w-4" />
-                Settings
+            <Link class="block w-full" :href="route(`${user.type}.profile.edit`)" prefetch as="button">
+            <Settings class="mr-2 h-4 w-4" />
+            Configurações
             </Link>
+
         </DropdownMenuItem>
     </DropdownMenuGroup>
     <DropdownMenuSeparator />
     <DropdownMenuItem :as-child="true">
         <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
-            <LogOut class="mr-2 h-4 w-4" />
-            Log out
+        <LogOut class="mr-2 h-4 w-4" />
+        Deslogar
         </Link>
     </DropdownMenuItem>
 </template>
